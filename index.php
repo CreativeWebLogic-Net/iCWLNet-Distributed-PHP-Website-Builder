6a9472c88a84a7fd50bb7db061471ac71b811cc5
danielruul78 <danielruul78@gmail.com>
2023-03-13 19:36:43 +1100

    Server Host

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-	ini_set( 'display_errors', '1' );
+	
 	session_start();
+	ini_set( 'display_errors', '1' );
 	include_once("classes/clsDCMS.php");
 	$cms=new DistributedCMS();
 	$cms->CommandInterface($_SERVER['REQUEST_URI']);
