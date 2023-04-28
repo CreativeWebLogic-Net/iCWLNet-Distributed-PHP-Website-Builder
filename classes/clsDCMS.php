f1610592dbb3cf47d0560f6f6f3a6f0663d7c70a
danielruul78 <danielruul78@gmail.com>
2023-04-28 15:48:57 +1000

    Update

--- classes/clsDCMS.php
+++ classes/clsDCMS.php
@@ -1,39 +1,103 @@
 <?php
 	class DistributedCMS{
-		var $RemoteServer="creativeweblogic.net/";
+		var $ConfigServer="https://git.creativeweblogic.net/Server-Config-File.html";
+		var $RemoteServer="access.sitemanage.info/";
 		var $BaseCacheDirectory="cache/";
 		var $BaseDomainCacheDirectory="../cache/";
+      	var $DomainCacheDirectory="";
 		var $current_dir="";
 		var $current_back_dir="";
 		var $LocalServer;
+		var $Current_File="";
+		var $Current_Full_Cached_File="";
 		var $RequestUnCachedFiles=true;
 		var $RemoteServerIP="142.132.144.12";
 		var $ForbiddenExtensions=array();
 		var $useragent="curl";
 		var $cookieFile = "cookies.txt";
 		var $guid="";
+		var $domain_folders=array("html","images","linked","cookies");
+		var $domain_folder_index="";
+		var $file_extensions=array();
+		var $current_file_extension=array();
+		var $Current_Full_Directories=array();
+		var $Current_Full_Files=array();
+		var $Current_Full_File_Index=0;
+		var $error_count=0;
 		
 		function __construct(){
-			
-			
-			$current_dir=pathinfo(__DIR__);
-			//print_r($current_dir);
-			$this->current_back_dir=$current_dir["dirname"].'/';
-			//print "=1=".$this->current_back_dir."="."-\n\n";
-			$this->current_dir=$current_dir['dirname'].'/'.$current_dir['basename']."/";
-			
-			$this->LocalServer=urlencode($_SERVER['HTTP_HOST']);
-			$this->BaseDomainCacheDirectory=$this->CacheDirectory().$this->slash_wrap($this->LocalServer)."/";
-			//print $this->BaseDomainCacheDirectory;
-			if (!file_exists($this->BaseDomainCacheDirectory)) {
-				if(!mkdir($this->BaseDomainCacheDirectory)){
-					echo "error"."-\n\n";
+          	$this->create_domain_folders();
+		}
+		
+		function create_domain_folders() 
+		{ 
+			$this->create_file_extensions();
+          	          
+          	if (!file_exists($this->DomainCacheDirectory)) {	
+              	if(!mkdir($this->DomainCacheDirectory)){
+                	$this->Error("\n 00111 error -|-".$this->DomainCacheDirectory."-|-\n\n",9);
+                	//echo "error".$current_folder."-\n\n";
+              	}else{
+                  	//$this->Error("\n 00111234 success -|-".$this->DomainCacheDirectory."-|-\n\n",9);
+              	}
+            }else{
+             	//$this->Error("\n 00111222 directory exists error -|-".$this->DomainCacheDirectory."-|-\n\n",6); 
+            }
+          
+          	foreach($this->domain_folders as $key=>$val){
+				if($this->BaseDomainCacheDirectory=="../cache/"){
+					$this->create_cache_variables();
+				}
+				$current_folder=$this->BaseDomainCacheDirectory.$val;
+              	//$this->Error("\n 00007 create_domain_folders |".$this->BaseDomainCacheDirectory."-Folder-".$current_folder."\n\n",6);
+				if (!file_exists($current_folder)) {
+					if(!mkdir($current_folder)){
+						$this->Error("\n 001 error -|-".$current_folder."-|-\n\n",9);
+						//echo "error".$current_folder."-\n\n";
+					}else{
+						$this->Current_Full_Directories[$key]=$current_folder;
+						//$this->Error("=20011=".$current_folder."-20011-\n\n",1);
+						//print "=20011=".$current_folder."-20011-\n\n";
+					}
 				}
 			}
-			
+		}
+		
+		function create_file_extensions() 
+		{ 
+			$this->create_cache_directories();
+          	$this->file_extensions[$this->domain_folders[0]]=array("html","htm","php","py","pl","ci","aspx","/");
+			$this->file_extensions[$this->domain_folders[1]]=array("jpg","png","gif","svg","tiff","eps","psd","ico");
+			$this->file_extensions[$this->domain_folders[2]]=array("css","js","xml","txt","csv");
+			$this->file_extensions[$this->domain_folders[3]]=array("txt");
+			$this->Current_Full_File_Index=0;
+		}
+		
+		function create_cache_directories() 
+		{ 
+			$this->get_app_directory();
+          	$this->create_server_constants();
+          	$this->DomainCacheDirectory=$this->CacheDirectory().$this->LocalServer;
+			$this->BaseDomainCacheDirectory=$this->CacheDirectory().$this->LocalServer."/";
+          	$this->Error("\n 002 Cache Directories -|-".$this->DomainCacheDirectory."-|-".$this->BaseDomainCacheDirectory." | -002 \n\n",2);
+			$this->create_server_constants();
+		}
+		
+		function get_app_directory() 
+		{ 
+			$current_dir=pathinfo(__DIR__);
+			$this->current_back_dir=$current_dir["dirname"].'/';
+			$this->current_dir=$current_dir['dirname'].'/'.$current_dir['basename']."/";
+		}
+		
+		function create_server_constants() 
+		{ 
+			$this->error_count=0;
+			$this->Current_File=$_SERVER['REQUEST_URI'];//$_SERVER['REQUEST_URI']
+			$this->LocalServer=$_SERVER['HTTP_HOST'];
+			$this->Error("\n Base Directory |".$this->BaseDomainCacheDirectory."-URI-".$this->Current_File."\n\n");
 			
 		}
-
 		
 		function make_guid ($length=32) 
 		{ 
@@ -56,20 +120,30 @@
 
 		
 		function set_cookie(){
-			
+			//$this->Error("=299=-\n\n".var_export($_SESSION)."=/200=-\n\n");
 			if(!isset($_SESSION['guid'])){
 				$this->guid=$this->make_guid();
 				$_SESSION['guid']=$this->guid;
 			}else{
 				$this->guid=$_SESSION['guid'];
 			}
-			//print_r($_SESSION);
-			$this->cookieFile = "cache/cookies/".$this->guid."-cookies.txt";
-			if(!file_exists($this->cookieFile)) {
-				$fh = fopen($this->cookieFile, "w");
-				fwrite($fh, "");
-				fclose($fh);
+			
+			//print "=200=-\n\n";
+			//print_r($this->Current_Full_Directories);
+			//print "=/200=-\n\n";
+			//$this->Error("=200=-\n\n".var_export($this->Current_Full_Directories)."=/200=-\n\n");
+			//$this->cookieFile = "cache/cookies/".$this->guid."-cookies.txt";
+			if(isset($this->Current_Full_Directories["cookies"])){
+				$this->cookieFile =$this->Current_Full_Directories["cookies"].$this->guid."-cookies.txt";
+				if(!file_exists($this->cookieFile)) {
+					$fh = fopen($this->cookieFile, "w");
+					fwrite($fh, "");
+					fclose($fh);
+				}
+			}else{
+				//$this->Error("=000003=-\n\n".var_export($this->Current_Full_Directories)."=000003=-\n\n");
 			}
+			
 		}
 		
 		function slash_wrap($DisplayPage){
@@ -83,31 +157,119 @@
 			return $this->current_back_dir.$this->BaseCacheDirectory;
 		}
 		function CheckIfHTMLFile($DisplayPage){
-			$BSlashEncoded=urlencode("/");
-			if(substr($DisplayPage,strlen($DisplayPage)-strlen($BSlashEncoded))==$BSlashEncoded){
-				return true;
+			//print "-00123\n\n".$DisplayPage."-\n\n";
+			$ret_val=false;
+			$BSlashEncoded='/';
+			$end_of_string=substr($DisplayPage,strlen($DisplayPage)-strlen($BSlashEncoded));
+			
+			if($end_of_string==$BSlashEncoded){
+				$ret_val=true;
+				$this->Error("-00123 true\n\n".$DisplayPage."-eos-".$end_of_string."-encoded-".$BSlashEncoded."-rv-".var_dump($ret_val)."\n\n");
+				//print "-00123 true\n\n".$DisplayPage."-eos-".$end_of_string."-encoded-".$BSlashEncoded."-rv-".var_dump($ret_val)."\n\n";
+			}else{
+				$ret_val=false;
+				$this->Error("-00123 false\n\n".$DisplayPage."-eos-".$end_of_string."-encoded-".$BSlashEncoded."-rv-".var_dump($ret_val)."\n\n");
+				//print "-00123 false\n\n".$DisplayPage."-eos-".$end_of_string."-encoded-".$BSlashEncoded."-rv-".var_dump($ret_val)."\n\n";
+			}
+			return $ret_val;
+		}
+		
+		function CheckFilesDuplicates($DisplayPage){
+			//print "-00123\n\n".$DisplayPage."-\n\n";
+			$ret_val=false;
+			foreach($this->Current_Full_Files as $current_index=>$values){
+				if($values["filename"]==$DisplayPage){
+					$ret_val=true;
+				}else{
+					$ret_val=false;
+				}
+				
+			}
+			return $ret_val;
+		}
+		
+		function Set_Full_Files($dimensions_array=array(),$files_index=0){
+			if(!$this->CheckFilesDuplicates($dimensions_array["filename"])){
+				if(count($dimensions_array)>0){
+					$current_index=count($this->Current_Full_Files);
+					$this->Current_Full_File_Index=$current_index;
+					$this->Current_Full_Files[$current_index]["filename"]=$dimensions_array["filename"];
+					$this->Current_Full_Files[$current_index]["encoded_filename"]=$dimensions_array["encoded_filename"];
+					$this->Current_Full_Files[$current_index]["extension"]=$dimensions_array["extension"];
+					$this->Current_Full_Files[$current_index]["extension_type"]=$dimensions_array["extension_type"];
+					$this->Current_Full_Files[$current_index]["directory"]=$dimensions_array["directory"];
+					$this->Current_Full_Files[$current_index]["complete_cache_location"]=$dimensions_array["directory"].$dimensions_array["encoded_filename"];
+					return false;
+				}else{
+					$this->Current_Full_File_Index=$files_index;
+					return $this->Current_Full_Files[$files_index];
+				}
 			}else{
 				return false;
 			}
+			
+		}
+		
+		function CheckFileDestination($DisplayPage){
+			//print "-00123\n\n".$DisplayPage."-\n\n";
+			$ret_val=false;
+			
+			foreach($this->file_extensions as $key=>$val){
+				//$this->Error("-00123\n\n".$key."-".var_export($val,true)."|\n\n",0);
+				//print "-00123\n\n".$key."-".var_export($val,true)."|\n\n";
+				
+				foreach($val as $ext_key=>$extension){
+					
+					//$extension="/";
+					$end_of_string=substr($DisplayPage,strlen($DisplayPage)-strlen($extension));
+					//print "-0010002\n\n".$end_of_string."-\n\n";
+					
+					if($end_of_string==$extension){
+						$array_dims["filename"]=$DisplayPage;
+						$array_dims["encoded_filename"]=$this->slash_wrap($DisplayPage);
+						$array_dims["extension"]=$extension;
+						$array_dims["extension_type"]=$key;
+						$array_dims["directory"]=$this->BaseDomainCacheDirectory.$key.$extension;
+                      	$array_dims["complete_cache_location"]=$this->BaseDomainCacheDirectory.$key.$array_dims["encoded_filename"];
+						
+						$this->Set_Full_Files($array_dims);
+						
+						$ret_val=true;
+					}else{
+						if(!$ret_val) $ret_val=false;
+					}
+					
+					
+				}
+				
+			}
+			
+			//$this->Error("-001234666\n\n".$key."-".var_export($this->Current_Full_Files,true)."|\n\n");
+			//print "-001234\n\n".$key."-".var_export($this->Current_Full_Files,true)."|\n\n";
+			//print "-0010002\n\n".var_export($this->Current_Full_Files)."-".$ret_val."\n\n";
+			return $ret_val;
 		}
 		
 		function LocalFileName($DisplayPage){
-			//print $this->current_dir;
-			if($this->CheckIfHTMLFile($DisplayPage)){
-				//$filename = $this->BaseDomainCacheDirectory.str_replace("/","",base64_encode($DisplayPage));
-				$filename = $this->BaseDomainCacheDirectory.$this->slash_wrap($DisplayPage);
+			//print "-0010101\n\n".$DisplayPage."-\n\n";
+			if($this->CheckFileDestination($DisplayPage)){
+				$filename =$this->Current_Full_Files[$this->Current_Full_File_Index]["encoded_filename"];
 			}else{
-				//$filename = $this->CacheDirectory().str_replace("/","",base64_encode($DisplayPage));
-				$filename = $this->CacheDirectory().$this->slash_wrap($DisplayPage);
+				$filename ="404 Error";
+              	header("Location: /404.shtml");
+              	exit();
 			}
 			
-			//return str_replace("/","",$filename);
-			//return urlencode($filename);
+			$this->Current_Full_Cached_File=$filename;
+			//$this->Error("\n\n002123-".var_export($this->Current_Full_Files)."-\n\n");
+			//print "\n\n002123-".var_export($this->Current_Full_Files)."-\n\n";
 			return $filename;
 		}
 		
 		function url_get_contents($url){//,$DisplayPage) {
 			//print $url;
+			$this->Error("\n 6679 The file | ".$url." | content legth \n",1);
+			
 			$this->set_cookie();
 			$encoded="";
 			if(count($_GET)>0){
@@ -169,34 +331,77 @@
 			}while(filesize($filename)==0||$SCount<10);
 			$this->DisplayCacheFile($DisplayPage);
 			*/
+			$this->Error("\n 667 The file | ".strlen($result)." | content legth \n",1);
 			return $result;
 		}
 		
 		function WriteCacheFile($DisplayPage,$content){
 			//$DisplayPage="xxx";
-			$filename = $this->LocalFileName($DisplayPage);
-			//print $filename."-\n\n";
+			$filename ="";
+          	$filename =$this->Current_Full_Files[$this->Current_Full_File_Index]["complete_cache_location"];
+			if(strlen($content)>0){
+              	if (!$fh = fopen($filename, "w")) {
+                     $this->Error("\n 666003 The file | ".$filename." | Cannot open file",1);
+                     exit;
+                }else{
+                	fwrite($fh, $content);
+					fclose($fh);
+                  	$this->Error("\n 006662 The file written | ".$filename." | content size | ".strlen($content)." | ",1);
+                }
+				//$filename = $this->LocalFileName($DisplayPage);
+				//$fh = fopen($filename, "w");
+				
+			}else{
+				$this->Error("\n 666 The file | ".$filename." | no content",1);
+			}
+			
+			/*
+			print "-0030\n\n".$filename."-\n\n";
 			if (is_writable($filename)) {
 				if (!$handle = fopen($filename, 'x')) {
+					print "-0030\n\n".$filename."-not opened\n\n";
 					 $this->Error("Cannot open file ($filename)");
 					 exit;
+				}else{
+					print "-00302\n\n".$filename."-\n\n";
 				}
 				if (fwrite($handle, $content) === FALSE) {
+					print "-0031\n\n".$filename."-not written\n\n";
 					$this->Error("Cannot write to file ($filename)");
 					exit;
+				}else{
+					print "-00311\n\n".$filename."-written\n\n";
 				}
 				fclose($handle);
 			} else {
+				print "-003111\n\n".$filename." Not Writable-\n\n";
 				//$this->Error("The file $filename is not writable");
 				//echo $filename." - The file $filename is not writable"."-\n\n";
 			}
+			*/
 		}
 		
 		function CheckIfCacheExists($DisplayPage){
 			$filename = $this->LocalFileName($DisplayPage);
 			//print "-3-".$filename."--";
+			$this->Error("-3-".$filename."--");
+			/*
 			if(file_exists($filename)){
 				return true;
+			}else{
+				return false;
+			}
+			*/
+			if($filename!=""){
+				if(file_exists($filename)){
+					if(filesize($filename)!=0){
+						return true;
+					}else{
+						return false;
+					}
+				}else{
+					return false;
+				}
 			}else{
 				return false;
 			}
@@ -204,28 +409,37 @@
 		
 		function DisplayCacheFile($DisplayPage){
 			$filename = $this->LocalFileName($DisplayPage);
-			//print $filename;
-			$handle = fopen($filename, "r");
-			$contents = fread($handle, filesize($filename));
-			//if(substr($DisplayPage,strlen($DisplayPage)-1)=="/"){
-			fclose($handle);
-			//print "-1-".$contents."-"."-\n\n";
-			if(strlen($contents)==0){
-				unlink($filename);
-				//$ContType="Content-Type: ".exec("file -bi '$filename'");
-				$ContType=mime_content_type($filename);
-				header($ContType);
-				//print $contents."-\n\n"."-\n\n";
-			}else{
-				//print "-".$contents."-\n\n"."-\n\n";
-				print $this->DisplayRealtime($DisplayPage);
+			print "\n-DCacheFile=".$filename."\n";
+			if($this->CheckIfCacheExists($DisplayPage)){
+				
+				$handle = fopen($filename, "r");
+				$contents = fread($handle, filesize($filename));
+				//if(substr($DisplayPage,strlen($DisplayPage)-1)=="/"){
+				fclose($handle);
+				//print "-1-".$contents."-"."-\n\n";
+				if(strlen($contents)==0){
+					unlink($filename);
+					//$ContType="Content-Type: ".exec("file -bi '$filename'");
+					$ContType=mime_content_type($filename);
+					header($ContType);
+					//print $contents."-\n\n"."-\n\n";
+				}else{
+					//print "-".$contents."-\n\n"."-\n\n";
+					print $this->DisplayRealtime($DisplayPage);
+				}
 			}
+			
 			
 			//print $ContType;
 		}
 		
-		function Error($error){
-			print $error;
+		function Error($error_text,$error_type=-1){
+			$line_number=__LINE__;
+			$this->error_count++;
+			$pval="\n | ".$this->error_count." | ".$line_number."-|-".$error_text."-|-".$error_type.=" | \n";
+			if($error_type>6){
+				print $pval;
+			};
 		}
 		
 		function IsValidFile($DisplayPage){
@@ -271,7 +485,7 @@
 			
 			if($this->IsValidFile($DisplayPage)){
 				//$DisplayPage=urlencode($DisplayPage);
-				$DisplayPage=$DisplayPage;
+				//$DisplayPage=$DisplayPage;
 				
 				if(!$this->CheckIfCacheExists($DisplayPage)){
 					
@@ -281,9 +495,11 @@
 					}else{
 						//echo"404"."-\n\n";	
 					}
-					//print "New Data\n\n"."-\n\n";
+					$this->Error("1234 New Data Page | ".$DisplayPage."\n\n"."-\n\n");
+					//print "1234 New Data Page | ".$DisplayPage."\n\n"."-\n\n";
 				}else{
 					$this->DisplayCacheFile($DisplayPage);
+					$this->Error("Retrieved From Cache\n\n");
 					//print "Retrieved From Cache\n\n";
 				}
 			}
